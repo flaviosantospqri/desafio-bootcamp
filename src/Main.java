275130d16cab8d016e800b0e9c8f0e696ee4dcfb
Flavio Santos <flavio.pasantos@outlook.com>
2023-01-02 16:45:13 -0300

    Desenvolvimento de funcionalidades

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.flavio.desafio.dominio.model.BootCamp;
 import br.com.flavio.desafio.dominio.model.Curso;
+import br.com.flavio.desafio.dominio.model.Dev;
 import br.com.flavio.desafio.dominio.model.Mentoria;
 
 import java.time.LocalDate;
@@ -12,5 +14,34 @@ public class Main {
         Mentoria m1 = new Mentoria("Java", "Mentoria Java", LocalDate.now());
 
         System.out.println(m1);
+
+        BootCamp bootCamp = new BootCamp();
+        bootCamp.setNome("Bootcamp Java");
+        bootCamp.setDescricao("Descricação");
+        bootCamp.getConteudos().add(c1);
+        bootCamp.getConteudos().add(c2);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Flávio");
+        dev1.inscreverBootCamp(bootCamp);
+        System.out.println("Conteúdo inscrito" + dev1.getConteudosInscritos());
+
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println("Conteúdo inscrito" + dev1.getConteudosInscritos());
+        System.out.println("Conteúdo concluidos" + dev1.getConteudosConcluidos());
+        System.out.println("xp" + dev1.calcularTotalXp());
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Joao");
+        dev2.inscreverBootCamp(bootCamp);
+        System.out.println("Conteúdo inscrito" + dev2.getConteudosInscritos());
+
+        dev2.progredir();
+        System.out.println("Conteúdo inscrito" + dev2.getConteudosInscritos());
+        System.out.println("Conteúdo concluidos" + dev2.getConteudosConcluidos());
+        System.out.println("xp" + dev2.calcularTotalXp());
+
+
     }
 }
--- src/br/com/flavio/desafio/dominio/model/Dev.java
+++ src/br/com/flavio/desafio/dominio/model/Dev.java
@@ -2,6 +2,7 @@ package br.com.flavio.desafio.dominio.model;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -9,11 +10,24 @@ public class Dev {
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootCamp(BootCamp bootCamp){}
+    public void inscreverBootCamp(BootCamp bootCamp){
+        this.conteudosInscritos.addAll(bootCamp.getConteudos());
+        bootCamp.getDevs().add(this);
+    }
 
-    public void progredir(){}
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        }else{
+            System.err.println("Você não está matricula em nenhum conteúdo.");
+        }
+    }
 
-    public void calcularTotalXp(){}
+    public double calcularTotalXp(){
+        return this.conteudosConcluidos.stream().mapToDouble(Conteudo::calcularXp).sum();
+    }
 
     public String getNome() {
         return nome;
