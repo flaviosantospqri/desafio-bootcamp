f18152922325bdc01ccfbce86fab5a875147462e
Flavio Santos <flavio.pasantos@outlook.com>
2023-01-02 14:53:19 -0300

    create model

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import br.com.flavio.desafio.dominio.model.Curso;
+import br.com.flavio.desafio.dominio.model.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso c1 = new Curso("Java", "Curso Java", 10);
+        Curso c2 = new Curso("Js", "Curso Js", 10);
+        System.out.println(c1);
+
+        Mentoria m1 = new Mentoria("Java", "Mentoria Java", LocalDate.now());
+
+        System.out.println(m1);
     }
 }
